fce736a532033a6e430f1e0b102e363dd62fd95f
oraimagini <mompyn@gmail.com>
2023-03-03 11:56:44 -0300

    add folder volumecontrol

---
+++ volumecontrol/cvm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# control music made with Rofi
+
+alta=$(echo 🕪 );
+media=$(echo 🕩 );
+baja=$(echo 🕨 );
+mute=$(echo 🗙 );
+fuerte=$(echo 🎝 );
+options="$alta\n$media\n$baja\n$mute\n$fuerte"
+
+selected="$(echo -e "$options" | rofi -show -dmenu -theme ./theme/v1-cvm.rasi)"
+
+case $selected in
+    "${alta}")
+        pactl set-sink-volume @DEFAULT_SINK@ 72%;
+        exit;;
+    "${media}")
+        pactl set-sink-volume @DEFAULT_SINK@ 42%
+        ;;
+    "${baja}")
+        pactl set-sink-volume @DEFAULT_SINK@ 13%
+        ;;
+    "${mute}")
+        pactl set-sink-volume @DEFAULT_SINK@ 0%
+        ;;
+    "${fuerte}")
+        pactl set-sink-volume @DEFAULT_SINK@ 100%
+        ;;
+esac
