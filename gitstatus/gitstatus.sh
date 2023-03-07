83ba42ca3cabe27a68d210b2c9d3bcea5baf3585
oraimagini <mompyn@gmail.com>
2023-03-07 17:24:10 -0300

    fix menor gitstatus.sh

--- gitstatus/gitstatus.sh
+++ gitstatus/gitstatus.sh
@@ -5,4 +5,10 @@ gsa=$(cd ~/workspace/arduino/ && git status | sed -n "4p" | sed -E 's/^.*(Change
 gse=$(cd ~/workspace/exercises/ && git status | sed -n "4p" | sed -E 's/^.*(Changes|clean|Untracked|limpio|Cambios).*$/\1/')
 gsc=$(cd ~/workspace/config/ && git status | sed -n "4p" | sed -E 's/^.*(Changes|clean|Untracked|limpio|Cambios).*$/\1/')
 
-echo " || Arduino: $gsa || Config: $gsc || Exercises: $gse" | rofi -sep '|' -dmenu -p " *** GIT STATUS *** " -theme ./theme/v1-gitstatus.rasi -i
+egsa=$(echo "    Arduino: $gsa");
+egse=$(echo "    Exercises: $gse");
+egsc=$(echo "    Config: $gsc");
+options="\n\n$egsa\n\n$egse\n\n$egsc\n\n"
+
+#echo " || Arduino: $gsa || Config: $gsc || Exercises: $gse" | rofi -sep '|' -dmenu -p " *** GIT STATUS *** " -theme ./theme/v1-gitstatus.rasi -i
+selected="$(echo -e "$options" | rofi -show -dmenu -p " *** GIT STATUS *** " -theme ./theme/v1-gitstatus.rasi -i)"
