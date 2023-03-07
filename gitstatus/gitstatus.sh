8b79ec22168d6e53a3896e45ec2d0dde18b88ed1
oraimagini <mompyn@gmail.com>
2023-03-07 17:02:31 -0300

    fix menor theme v1-gitstatus

--- gitstatus/gitstatus.sh
+++ gitstatus/gitstatus.sh
@@ -1,8 +1,8 @@
- #!/usr/bin/bash
+#!/usr/bin/bash
 
 # var git status
 gsa=$(cd ~/workspace/arduino/ && git status | sed -n "4p" | sed -E 's/^.*(Changes|clean|Untracked|limpio|Cambios).*$/\1/')
 gse=$(cd ~/workspace/exercises/ && git status | sed -n "4p" | sed -E 's/^.*(Changes|clean|Untracked|limpio|Cambios).*$/\1/')
 gsc=$(cd ~/workspace/config/ && git status | sed -n "4p" | sed -E 's/^.*(Changes|clean|Untracked|limpio|Cambios).*$/\1/')
 
-echo " || Arduino: $gsa || Config: $gsc || Exercises: $gse" | rofi -sep '|' -dmenu -p " *** GIT STATUS *** " -theme ~/.config/rofi/theme/sidev4-r.rasi -i
+echo " || Arduino: $gsa || Config: $gsc || Exercises: $gse" | rofi -sep '|' -dmenu -p " *** GIT STATUS *** " -theme ./theme/v1-gitstatus.rasi -i
