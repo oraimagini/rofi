1e92f74cc0e62387a397eee1f90fe8bd3a3b0dc4
oraimagini <mompyn@gmail.com>
2023-04-23 18:32:17 -0400

    change menor file lsr.sh

--- scripts/lsr.sh
+++ scripts/lsr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls ~/workspace/config/bash/bsct | rofi -show -dmenu -p "> " -theme ./theme/lsr.rasi -i | xargs -I_ bash ~/workspace/config/bash/bsct/_
+ls ~/workspace/config/bash/bsct | rofi -show -dmenu -p "> " -theme ./theme/style01/lsr.rasi -i | xargs -I_ bash ~/workspace/config/bash/bsct/_
