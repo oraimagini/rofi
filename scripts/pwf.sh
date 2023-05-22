1086ddc1dd659977e4d0e48bf56a89ac9a8e13a4
oraimagini <mompyn@gmail.com>
2023-05-22 11:14:56 -0400

    change menor rofi

---
+++ scripts/pwf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Powermenu made with Rofi
+
+poweroff=$(echo ⏻ );
+reboot=$(echo ◀ );
+sleep=$(echo ⏾ );
+logout=$(echo ❮);
+lock=$(echo ✖);
+options="$poweroff\n$reboot\n$sleep\n$logout\n$lock"
+
+selected="$(echo -e "$options" | rofi -show -dmenu -theme ~/.config/rofi/theme/style03/launcher.rasi )"
+
+case $selected in
+    "${poweroff}")
+        systemctl poweroff
+        ;;
+    "${reboot}")
+        systemctl reboot
+        ;;
+    "${sleep}")
+        systemctl suspend
+        ;;
+    "${logout}")
+        kill -9 -1
+        ;;
+    "${lock}")
+	terminator -f -x "cmatrix; bash" && terminator --geometry=1200x780+0 -x "cmatrix; bash"
+        ;;
+esac
